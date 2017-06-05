688fe3e9bfa210d8cffb630a471629cab441771e
Jeremy Black <jb@yodacom.com>
2017-06-05 07:47:56 -0600

    server added

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const config = require("config.js");
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+app.use(bodyParser.json());
+
+const runServer = function(callback) {
+    mongoose.connect(config.DATABASE_URL, function(err) {
+        if (err && callback) {
+            return callback(err);
+        }
+
+        app.listen(config.PORT, function() {
+            console.log('Listening on localhost:' + config.PORT);
+            if (callback) {
+                callback();
+            }
+        });
+    });
+};
+
+if (require.main === module) {
+    runServer(function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
 
 exports.app = app;
-exports.storage = storage;
+exports.runServer = runServer;
